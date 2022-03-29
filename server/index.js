b33289964b4a00cfabbf3ddf5bd59c9218f06ef9
Evan <evjava@gmail.com>
2022-03-28 23:52:40 -0400

    added document autogen

--- server/index.js
+++ server/index.js
@@ -1,13 +1,51 @@
 //App setup and imports
 require("dotenv").config();
 const { default: mongoose } = require("mongoose");
-// const { app } = require("./setup");
+const { app } = require("./setup");
 const morgan = require("morgan")
 const express = require("express");
-const app = express();
+
 const { getUserIdFromToken } = require("./middleware/getUserFromToken");
 
 const port = process.env.PORT;
+
+
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Express API for JSONPlaceholder',
+    version: '1.0.0',
+    description:
+      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
+    license: {
+      name: 'Licensed Under MIT',
+      url: 'https://spdx.org/licenses/MIT.html',
+    },
+    contact: {
+      name: 'JSONPlaceholder',
+      url: 'https://jsonplaceholder.typicode.com',
+    },
+  },
+  servers: [
+    {
+      url: 'http://localhost:3000',
+      description: 'Development server',
+    },
+  ],
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+
+
+const swaggerSpec = swaggerJSDoc(options);
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use(morgan('tiny'));
 app.use(express.json());
@@ -25,7 +63,10 @@ app.use(function (req, res, next) {
   next();
 });
 
+
+
 app.use(getUserIdFromToken);
+
 app.use("/api/users", require("./routes/users"));
 app.use("/api/userProfiles", require("./routes/userProfiles"));
 
--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,34 @@ const express = require("express");
 const { userController } = require("../controllers/userController");
 const router = express.Router();
 
-// router.get('/', userController.getUsers);
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of JSONPlaceholder users.
+ *     description: Retrieve a list of users from JSONPlaceholder. Can be used to populate a list of fake users when prototyping or testing an API.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 data:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       name:
+ *                         type: string
+ *                         description: The user's name.
+ *                         example: Leanne Graham
+ */
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 router.post("/changePassword", userController.changePassword);
