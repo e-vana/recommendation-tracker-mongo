63068cb29796938c5d6c079d7585c655ad5926bf
Evan <evjava@gmail.com>
2022-04-01 15:45:58 -0400

    added placeholder files

---
+++ server/services/email.js
@@ -0,0 +1,2 @@
+//Placeholder for the code that will send and generate emails
+// https://documentation.mailgun.com/en/latest/quickstart-sending.html#how-to-start-sending-email
---
+++ server/services/schedule.js
@@ -0,0 +1,2 @@
+//placeholder for the code that will generate cron jobs for the alerting system
+// https://www.npmjs.com/package/node-cron
